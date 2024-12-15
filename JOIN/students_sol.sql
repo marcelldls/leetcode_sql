dfcfe0eadf534bb2cadd48f86ece7930c2a314a3
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-12-15 15:54:42 +0000

    Add more JOIN

---
+++ JOIN/students_sol.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT s.student_id, s.student_name, j.subject_name, COUNT(e.subject_name) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects j
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND j.subject_name = e.subject_name
+GROUP BY j.subject_name, s.student_id, s.student_name
+ORDER BY s.student_id, j.subject_name 
