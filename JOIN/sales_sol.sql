165f44bb917acbf92e5d614fa1282869d9875841
Marcell Douglas Nagy <133755508+marcelldls@users.noreply.github.com>
2024-12-24 08:16:54 +0000

    Update sales_sol.sql

--- JOIN/sales_sol.sql
+++ JOIN/sales_sol.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
-SELECT Product.product_name, Sales.year, Sales.price FROM Sales
-JOIN Product ON Sales.product_id=Product.product_id;
+SELECT p.product_name, s.year, s.price FROM Sales AS s
+LEFT JOIN Product AS p
+ON p.product_id=s.product_id;
