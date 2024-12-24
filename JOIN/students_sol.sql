b54bccc9fa3d38e0583bb8112fa0541489bee6f8
Marcell Douglas Nagy <133755508+marcelldls@users.noreply.github.com>
2024-12-24 16:48:22 +0000

    Update students_sol.sql

--- JOIN/students_sol.sql
+++ JOIN/students_sol.sql
@@ -3,5 +3,5 @@ SELECT s.student_id, s.student_name, j.subject_name, COUNT(e.subject_name) AS at
 FROM Students s
 CROSS JOIN Subjects j
 LEFT JOIN Examinations e ON s.student_id = e.student_id AND j.subject_name = e.subject_name
-GROUP BY j.subject_name, s.student_id, s.student_name
-ORDER BY s.student_id, j.subject_name 
+GROUP BY s.student_id, j.subject_name
+ORDER BY s.student_id, j.subject_name;
