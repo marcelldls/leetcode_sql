6085b003c73244893b3f1b68bf409392b2e75159
Marcell Douglas Nagy <133755508+marcelldls@users.noreply.github.com>
2024-12-26 09:36:57 +0000

    Update reports_sol.sql

--- JOIN/reports_sol.sql
+++ JOIN/reports_sol.sql
@@ -2,6 +2,5 @@
 SELECT e2.name FROM Employee e1
 LEFT JOIN Employee e2 
 ON e1.managerId=e2.id
-WHERE e2.name is NOT NULL
-GROUP BY e2.name
-HAVING COUNT(e2.name)>5;
+GROUP BY e2.id
+HAVING COUNT(e2.id)>=5;
