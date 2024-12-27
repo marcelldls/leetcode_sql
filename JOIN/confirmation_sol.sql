1bea0697f2a9402fcc9b0303ab32aea9bf308fa9
Marcell Douglas Nagy <133755508+marcelldls@users.noreply.github.com>
2024-12-27 11:59:03 +0000

    Create confirmation_sol.sql

---
+++ JOIN/confirmation_sol.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT s.user_id as user_id, ROUND(IFNULL(AVG(c.action='confirmed'),0),2) as confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id=c.user_id
+GROUP BY s.user_id;
